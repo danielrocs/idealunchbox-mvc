01221abd0742d1ef91dd585ba068da1e9cc2efa8
Daniel Rocha <danielrocsou@gmail.com>
2019-02-24 09:51:21 -0800

    Start

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,10 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://idealunchbox.eastus2.cloudapp.azure.com',
+      'https://idealunchbox.eastus2.cloudapp.azure.com',
+    ],
 
 
     /***************************************************************************
@@ -371,7 +371,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'https://idealunchbox.eastus2.cloudapp.azure.com',
     internalEmailAddress: 'support@example.com',
 
     // mailgunDomain: 'mg.example.com',
